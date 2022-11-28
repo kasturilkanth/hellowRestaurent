73ec234a1811f6c3b154be8adfd1d997ef2c3755
kasturi l <kasturilkanth@yahoo.com>
2022-11-28 10:33:29 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import "./styles.css";
 
 export default function App() {
   return (
+  
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    
+<div class="header">
+  <h1>Chania</h1>
+</div>
+
+<div class="row">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+      <li>The Flight</li>
+      <li>The City</li>
+      <li>The Island</li>
+      <li>The Food</li>
+    </ul>
+  </div>
     </div>
+  
   );
 }
